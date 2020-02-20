4f181c61ecff7667fea70e969150ff42980c7a97
sgulls <abaoagjoojin@outlook.com>
2020-02-20 17:01:17 -0500

    remove old libc libs

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export TZ=UTC
+export TZ=EST5EDT4,M4.1.0/02:00:00,M10.5.0/02:00:00
 
 # make tools/install
 # make -j4 toolchain/install
 # make -j4 target/compile
-PATH=/home/voxel/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+# PATH=/home/voxel/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 make V=99 >& report
